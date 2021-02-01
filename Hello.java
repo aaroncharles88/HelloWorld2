00c7e6f7c940606010919b733d5d185e06451a5d
Aaron Lovely <aaron.lovely@maine.edu>
2021-02-01 16:33:06 -0500

    AliceBranch update

--- Hello.java
+++ Hello.java
@@ -6,5 +6,7 @@ public class Hello {
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World.");
 		System.out.println("See you later!");
+		
+		System.out.println("Hi Alice");
 	}
 }
