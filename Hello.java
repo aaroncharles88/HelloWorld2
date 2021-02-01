27346c7d9d9da52efc658c549365e385d92eac2f
Aaron Lovely <aaron.lovely@maine.edu>
2021-02-01 15:18:04 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ package hello;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World.");
 	}
 }
